692813c8233468ea6f0ac3d78d0f009b1254a3f2
BaoAnh020603 <143186266+BaoAnh020603@users.noreply.github.com>
2025-01-08 23:35:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn form gửi dữ liệu đi mặc định
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Kiểm tra tài khoản đăng nhập
+    if (username === "admin" && password === "123456") {
+        alert("Đăng nhập thành công!");
+        window.location.href = "index.html"; // Chuyển hướng đến trang chính
+    } else {
+        alert("Tên đăng nhập hoặc mật khẩu không đúng!");
+    }
+});
 
