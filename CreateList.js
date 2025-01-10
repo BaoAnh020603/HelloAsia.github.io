e7b9059118e86f799b4814750955c44d5dc8938a
BaoAnh020603 <143186266+BaoAnh020603@users.noreply.github.com>
2025-01-10 17:15:38 +0700

    Update CreateList.js

--- CreateList.js
+++ CreateList.js
@@ -1,16 +1,1 @@
- <!-- JavaScript -->
-    <script>
-        function openModal() {
-            document.getElementById('createListModal').classList.add('active');
-        }
 
-        function closeModal() {
-            document.getElementById('createListModal').classList.remove('active');
-        }
-
-        document.getElementById('create-list-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            alert('Danh sách của bạn đã được lưu!');
-            closeModal();
-        });
-    </script>
